77d0941222f0e825b39108ce75efffd327cdb72a
realsoc <hugo.djemaa@gmail.com>
2015-03-16 14:25:31 +0100

    HomeView init

---
+++ app/views/HomeView.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<title>Accueil</title>
+	</head>
+	<body>
+		<header>
+			<h1>This is the Main View</h1>
+		</header>
+	</body>
+</html>
