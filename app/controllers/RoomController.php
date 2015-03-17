a0287b1404fae2c51fd032a7733cd52e2ba42119
realsoc <hugo.djemaa@gmail.com>
2015-03-17 21:53:26 +0100

    Home and Room controllers funcs and route

---
+++ app/controllers/RoomController.php
@@ -0,0 +1,21 @@
+<?php
+class RoomController extends Controller
+{
+
+	public function index()
+	{
+		$this->view('Room');
+	}
+	public function sendMessage(){
+
+	}
+	public function quitRoom(){
+		
+	}
+	public function getMessages(){
+
+	}
+	public function getUsers(){
+		
+	}
+}
